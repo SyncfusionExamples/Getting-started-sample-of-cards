44f61a592f18ef71d2f9da37dfefdfb3f5a91c1d
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-06-08 16:18:04 +0530

    Sample included

---
+++ Getting_Started/Getting_Started/Getting_Started/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.XForms.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Getting_Started
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
